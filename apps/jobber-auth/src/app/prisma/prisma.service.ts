f8cfd30e4af16881fd709e926efd3c34ad04063e
igor_rudakov30 <irudakov@ezfacility.com>
2025-12-10 14:19:30 +0200

    Minor prisma config clean up

--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -1,13 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma-clients/jobber-auth';
-
 import { PrismaPg } from '@prisma/adapter-pg';
-import * as pg from 'pg';
 
-const connectionString = process.env.AUTH_DATABASE_URL;
-
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({
+  connectionString: process.env.AUTH_DATABASE_URL,
+});
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
