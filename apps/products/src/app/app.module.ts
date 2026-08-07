9280b51fac8130cf82536c1605451a3965d292da
Igor Rudakov <igorxlib@gmail.com>
2026-08-07 23:03:57 +0300

    Added products module and drizzle orm config

---
+++ apps/products/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { LoggerModule } from '@jobber/nestjs';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProductsModule,
+  ],
+})
+export class AppModule {}
---
+++ apps/products/src/main.ts
@@ -0,0 +1,16 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { init } from '@jobber/nestjs';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  await init(app);
+}
+
+bootstrap();
